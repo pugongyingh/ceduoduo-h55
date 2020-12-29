50038f38582ea7093f6cc3af7a37c2ca39763af4
pugongyingh <pgyhh@sina.cn>
2020-12-29 15:48:06 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -22,7 +22,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: "localhost", // can be overwritten by process.env.HOST
+    host: "116.63.230.153", // can be overwritten by process.env.HOST
     port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     errorOverlay: true,
     notifyOnErrors: true,
